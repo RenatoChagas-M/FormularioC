1b9c6edf7bba38e31e30ca7cda9795e525d81857
Jaotarzan <joavssouza59@gmail.com>
2024-07-24 21:21:34 -0300

    first commit

---
+++ src/components/forms.vue
@@ -0,0 +1,18 @@
+<script setup>
+import InputForm from './inputForm.vue'
+
+
+function save() {
+  e.preventDefault()
+  console.log('foi')
+}
+</script>
+
+<template>
+  <form :action="save">
+    <InputForm type="number" max_length="4" required="required" />
+    <input type="submit" />
+  </form>
+</template>
+
+<style scoped></style>
